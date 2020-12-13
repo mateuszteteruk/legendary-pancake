e643640fe0aa98e682ae471028f68594c0365e75
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2020-12-13 18:47:33 +0100

    Add toolbar

--- app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
@@ -2,6 +2,11 @@ package pl.mateuszteteruk.composeplayground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import pl.mateuszteteruk.composeplayground.recipes.RecipeList
 import pl.mateuszteteruk.composeplayground.recipes.defaultRecipes
@@ -11,7 +16,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecipeList(recipes = defaultRecipes)
+            Column(modifier = Modifier.fillMaxSize()) {
+                TopAppBar(title = {
+                    Text(text = "Recipes")
+                })
+                RecipeList(recipes = defaultRecipes)
+            }
         }
     }
 }
