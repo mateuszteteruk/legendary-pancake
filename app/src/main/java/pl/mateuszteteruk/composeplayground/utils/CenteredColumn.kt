0d79946927fd281391b61290f690018f6240a2ea
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2020-12-17 19:35:34 +0100

    Create CenteredColumn

---
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/utils/CenteredColumn.kt
@@ -0,0 +1,20 @@
+package pl.mateuszteteruk.composeplayground.utils
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CenteredColumn(composable: @Composable () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        composable()
+    }
+}
