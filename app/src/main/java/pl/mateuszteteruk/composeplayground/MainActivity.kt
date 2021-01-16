37db58f4f0f049f8d414c7dd253fc14314857c26
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2021-01-16 14:06:13 +0100

    Update MainActivity.kt to show button

--- app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
@@ -2,25 +2,20 @@ package pl.mateuszteteruk.composeplayground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import pl.mateuszteteruk.composeplayground.recipes.RecipeList
-import pl.mateuszteteruk.composeplayground.recipes.defaultRecipes
+import pl.mateuszteteruk.composeplayground.animatedfavbutton.AnimatedFavouriteButton
+import pl.mateuszteteruk.composeplayground.ui.ComposePlaygroundTheme
+import pl.mateuszteteruk.composeplayground.utils.CenteredColumn
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize()) {
-                TopAppBar(title = {
-                    Text(text = "Recipes")
-                })
-                RecipeList(recipes = defaultRecipes)
+            ComposePlaygroundTheme {
+                CenteredColumn {
+                    AnimatedFavouriteButton()
+                }
             }
         }
     }
