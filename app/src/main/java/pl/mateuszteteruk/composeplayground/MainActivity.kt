fa780cf6d4aff47c7904a13c70f5e0d2b76297fb
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2021-10-14 16:02:09 +0200

    Base app bar with lazy column

--- app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
@@ -3,9 +3,8 @@ package pl.mateuszteteruk.composeplayground
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import pl.mateuszteteruk.composeplayground.animatedfavbutton.AnimatedFavouriteButton
+import pl.mateuszteteruk.composeplayground.lazyliststate.LazyListStateExample
 import pl.mateuszteteruk.composeplayground.ui.ComposePlaygroundTheme
-import pl.mateuszteteruk.composeplayground.utils.CenteredColumn
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
-                CenteredColumn {
-                    AnimatedFavouriteButton()
-                }
+                LazyListStateExample()
             }
         }
     }
---
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/lazyliststate/LazyListStateExample.kt
@@ -0,0 +1,31 @@
+package pl.mateuszteteruk.composeplayground.lazyliststate
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LazyListStateExample() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar {
+            Text(text = "LazyListStateExample")
+        }
+        LazyColumn {
+            items(50) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    text = "Item #$it"
+                )
+            }
+        }
+    }
+}
