59fb81519781a0c32861154ca2dc2ee3d9cd9843
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2021-10-16 16:17:31 +0200

    Add example for elevation simple

--- app/src/main/java/pl/mateuszteteruk/composeplayground/lazyliststate/LazyListStateExample.kt
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/lazyliststate/LazyListStateExample.kt
@@ -5,19 +5,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun LazyListStateExample() {
+    val state: LazyListState = rememberLazyListState()
     Column(modifier = Modifier.fillMaxSize()) {
-        TopAppBar {
-            Text(text = "LazyListStateExample",modifier=Modifier.padding(8.dp))
+        TopAppBar(
+            backgroundColor = MaterialTheme.colors.surface,
+            elevation = state.elevationSimple
+        ) {
+            Text(text = "LazyListStateExample", modifier = Modifier.padding(8.dp))
         }
-        LazyColumn {
+        LazyColumn(
+            state = state,
+        ) {
             items(50) {
                 Text(
                     modifier = Modifier
@@ -29,3 +39,12 @@ fun LazyListStateExample() {
         }
     }
 }
+
+private val LazyListState.elevationSimple: Dp
+    get() = if (firstVisibleItemIndex == 0) {
+        minOf(firstVisibleItemScrollOffset.toFloat().dp / 3, LIFTED_ELEVATION)
+    } else {
+        LIFTED_ELEVATION
+    }
+
+private val LIFTED_ELEVATION = 24.dp
