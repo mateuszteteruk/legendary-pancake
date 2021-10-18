1bbe993af25f285d436473cc341b772b1981dae2
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2021-10-18 09:06:52 +0200

    Currently selected month indicator on list

--- app/src/main/java/pl/mateuszteteruk/composeplayground/dropdownmenu/DropdownMenuExample.kt
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/dropdownmenu/DropdownMenuExample.kt
@@ -1,16 +1,26 @@
 package pl.mateuszteteruk.composeplayground.dropdownmenu
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
 import androidx.compose.material.ExposedDropdownMenuDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 data class Month(val name: String)
 
@@ -52,13 +62,21 @@ fun DropdownMenuExample() {
             onDismissRequest = { expanded = false }
         ) {
             months.forEach { month ->
+                val isCurrentlySelected = month == selectedMonth
                 DropdownMenuItem(
                     onClick = {
                         selectedMonth = month
                         expanded = false
                     }
                 ) {
-                    Text(text = month.name)
+                    Row {
+                        if (isCurrentlySelected) {
+                            Icon(imageVector = Icons.Outlined.Check, contentDescription = "Selected month")
+                        } else {
+                            Spacer(modifier = Modifier.width(24.dp))
+                        }
+                        Text(text = month.name)
+                    }
                 }
             }
         }
