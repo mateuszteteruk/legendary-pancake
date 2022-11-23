ea7f768d2c7f2f8e30a95b13de83e02c7b7074e4
Mateusz Teteruk <mateusz.teteruk@gmail.com>
2022-11-23 15:10:20 +0100

    Add tilt modifier demo

--- app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
+++ app/src/main/java/pl/mateuszteteruk/composeplayground/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import pl.mateuszteteruk.composeplayground.navigation.SongsNavigation
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import pl.mateuszteteruk.composeplayground.modifiers.tiltOnTouch
 import pl.mateuszteteruk.composeplayground.ui.ComposePlaygroundTheme
 
 class MainActivity : AppCompatActivity() {
@@ -21,13 +30,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
                 ) {
-                    SongsNavigation()
+                    Box(
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Card(
+                            modifier = Modifier
+                                .size(width = 300.dp, height = 400.dp)
+                                .tiltOnTouch(),
+                            shape = RoundedCornerShape(24.dp),
+                            border = BorderStroke(2.dp, Color.Black)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.smoothie),
+                                contentScale = ContentScale.FillWidth,
+                                contentDescription = "",
+                            )
+                        }
+                    }
                 }
             }
         }
